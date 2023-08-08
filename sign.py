34b54cad8e8ee116d2a0755b9443784303710de8
Michał Górny <mgorny@gentoo.org>
2023-08-08 11:24:48 +0200

    Hide PGP and ASCII art from screen readers
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- sign.py
+++ sign.py
@@ -7,19 +7,20 @@ def main():
     with open('index.html', 'r') as f:
         data = f.read()
 
-    first_pre = data.find('<pre>')
+    pre_elem = '<pre aria-hidden="true">'
+    first_pre = data.find(pre_elem)
     first_pre_end = data.find('</pre>')
-    last_pre = data.rfind('<pre>')
+    last_pre = data.rfind(pre_elem)
     last_pre_end = data.rfind('</pre>')
 
-    body = data[first_pre_end:last_pre+len('<pre>')]
+    body = data[first_pre_end:last_pre+len(pre_elem)]
     gpg = subprocess.Popen(['gpg', '--clearsign', '-'],
             stdin=subprocess.PIPE,
             stdout=subprocess.PIPE)
     stdout, stderr = gpg.communicate(body.encode('utf8'))
     new_body = stdout.decode('utf8')
 
-    new_data = data[:first_pre+len('<pre>\n')] + new_body + data[last_pre_end:]
+    new_data = data[:first_pre+len(f'{pre_elem}\n')] + new_body + data[last_pre_end:]
 
     with open('index.html', 'w') as f:
         f.write(new_data)
